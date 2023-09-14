d24d48c1fa0d0204b22c2b798b66474e8e951733
yikes <79975300+dotjoma@users.noreply.github.com>
2023-09-14 09:28:32 +0900

    Add files via upload

---
+++ Auto_Plant.lua
@@ -0,0 +1,35 @@
+SsknLg582h3DOVbyT7sn922x92GLTv2SxHBSbyurbqxGuqasruqQnLkmSYg/MbQJVLQAELpXbu7uXomm5xKuqA==
+-----------------------------------------------------------------------------------------------------------
+BotIndex = 0
+blockID = pepper --Put here the farmable you have.
+takeX=90 --Coordinate x take seed
+takeY=23 --Coordinate y take seed
+------------------------------------Code Area---------------------------------------------------------
+RDV7s6tvs8eiY9/hmd4NabRYfGdwwzNXKaCLMjmJEYmmI0uDwIBCtcv4s7
+RQFWrR8cagCQTnV5xts3sbzikz9DW9g5X+7uynR94EcxVvPyTspdia60bZF
+SWpNAMNekLi65QnVUFdCy7uXNG76w98A8YllWbeayxD6Osz3GL4NX9vZ
+GxmQJ4Su+BcRQkEbPECmQ8WAwZqYjI4VOi2UqUFmEW0rue7InMInjfBe
+4nfTcHquF/qURvePfMFMYkM7JLDaJF3rdk3Y5m7tpz3rRiA4h+IQOPaDP5dT
+7LBs+RUKkChOsg3lUz/YJnjIasPVvJlHOC1qu+fN7/e6jgiCWkRnn/29QFgV5F
+/NLq8mAcyDMzsPpA0/j63vSvYLMoCwWjWGimYQlu8Um8aE7qpnxIyqDzM
+6i2/OJS/+8eit1NeGRsuNkZBSwnvmWyPQ8xMud0lQcHrrC1O7osBeoVBSR2
+/rD2BBIbxtHT6ctTxGwscAo+00QH2Jk6g0Nc9ahgn9a+fr/GXIuJucl17b8y8oqZ
+mrCe6GFvdd+pazdVArQXQTU1PHqRBHyE8IaSxAAemtmLdIpLNP9v3mwu2F
+R6GTHDJRLt+xax7BpOJbizRdxhT0Glm5UUk97RLmQKv0IoHh4nzg/RN+kmD
+ufVN1x/KECgAo6OlmMnxLQ/2AKhrfivmMBKdLedxBJqCj+vwFFgD64rG1MiG
+RYrCyZ6qqQrZwXcPUwODrpqzQK96q3Go/rOzhT9zgWZFhUs9CA+BXIOhoJV
+d/elirf3yks0vZ9cfK0B1/Wiko9GR8CMruDNpKsOPYXSyehfNKCFtCpRwhfqIcIe9
+Jz/YCTddvqr+qJMwr5/anna0g5oWtnTmONgDXg/WSebTLNP4rRazZqVyKJw/1
+G4wHbAIHlTTSxzHrTlakhI9IHLVDVPuU0kmfC6aixHXwqqdybVJNCXJ5ywTsDtQ
+UibBCAbx0cbHk/uuXdKFa4HWe4wTeHEzbukNSw5Imw+FdUx+Zs1fB5+mvRZ
+FCFT5GlujHsOIh3eNqs1C+4SPRxSig8SjyVsRNBEtQdtCxX3cKXvDXPcdFil6iN
+VwdXQoExUxScqCeAnFdA5Eeb3wIFpaDzRBqYiwdOmSxyZAm5ggbwaVdAD
+dvdi3mRY53gL/GjVEOthd6m6wSnK528G4XzJQkATtj4XyCUgFIMEvniwtqMk5vj
+VmzWIYOYui7as6Kblq1jX/kaDZOLbZOsZL9RckqmZzrdOHXvuKsE5bk3QBRIUr
+mThg7oSUq3bUa1FOEjZFeClEQvF3C6TBnvWFzuJTL+4M2MFj/FzK+wv27t8Cg
+2YTPfZxeJIKYK67CMRx/SZ8Aw4aU/eFscqCaghwTVaozxFUXuKs6fgIYPm2iDM
+Sp8/ganGyb4kMchNdUH8DfDqam5JOigL6BNr5O28ZwYIFWefjc7Ud4wZeGxtSIO
+6QYYdqVOKjPZnfvjcLy1I1vu54YCav3kY+YKuOkqfJk/xaBSktHB0P2irrgNFOaso
+V1Mxmrfl/QmIaXQeEyFnUpnyoo10LxXIhxZt+3F59UGoI5Ddp8xqy41OWW5wSK
+UpIgyzB9PrXFnO8DH8vs7wq8KlA9+29pCOv/Bxi78k9dM73+hVz/Sw614ADCbIQ
+cO4L4LsnvX/acpt8n+XwCsPiQ1SGvEzNrl8sHyPIS1edA08z3Q==
