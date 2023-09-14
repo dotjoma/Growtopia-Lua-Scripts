57b15b6b0ce46a807ea6d66b0646a1bfb4b4e963
yikes <79975300+dotjoma@users.noreply.github.com>
2023-09-14 09:13:53 +0900

    Add files via upload

---
+++ Auto_Take&Wear_Pickaxe.lua
@@ -0,0 +1,25 @@
+
+storageWorld = "" --Your storage world.
+doorID = "" --The door ID in your storage world.
+botIndex = 0 --You can find the bot index in ~ BOT -> INFO
+htbSpot = "EXIT" --Optional
+----------------------------Code Area----------------------------------
+u708Odbwq5ScTmI83nRkkDjNmaNpfyKaC63av1rNaeKS0NXk3A2EYwWrNbJsUSuKr+AO+
+Nic5LMdSZcGn+J7kWNhhPlm7KZgT90n/8L/V2r7n12j1b/DNrr3M44lMwM9Wf+c2UEDwRwd
+lBEPQeANZN9LKnkg5XRGk0KmvA0MBTqrxDFaICQ58YXPKU81rMekImTetplcZ08Gg9cnpd
+rZEX2nGF1mxus+VRYW2tTQkqFwGCdQMR5ozbcMQVrRc8e//AE6NB+tuPlK87HSWy4+h
+RbeDvQKXw4/+w3TC3zFEq5XkoIHgPLzt/Z0aRbM7WEyoocEJw6xGezDDOCEBHVUUD/4C
+Co/ez3IQceVJYgeUC9uVjNtY/VHiRRdY22jxBey/O8DSdZZ+NUgdOZKPj4xCIU+GczNChmQ
+Cmtis9d2UccQ0Hq+hjZoP7IBkCTlEJC8jtbeC9VTHPfJjZ7KzGlV17c29Yow0Fwbju6U7yBqZg
+BLbYe2youhakRCDww2MbrPwKWcJFPOPlfWLeCQWVdYplzIZWkBF+vLmUzXa84xSpk39
+67ih2VepSE2N+EX7gXFpvGrfaXaHjTRdJ4If2F7UsoV2Wzh0UBwTB82xjbI5AfMBUKH/UBOm
+W54LXg6rGDucQ91xAGcpiFPBnIVx4aHnyJy64mq/5JPFWFEp9Wl7CRf+augNzYjqm9iKZ5
+Xl2Q9bx0slx4M9ntBXyLGwhzxKfFbIfrq0TPRhUctAI05iZ4EhdPvBMMvARfD+H1S1Ez7MZtzt
+qAaGD+lOM9xxub7ddyMqO4F43IxhOJVJxYRx5tc3WBjZAvay7AH17LwZAU8O7Brvw0jXBg
+WgfY3NwTHTztBW9nj2W8nHcYVf9NH6DX4DqxDy2sIAjuhy7d5t9pAhrPeHwJKkshgSdgh1d
+fKxp6FC9IFQRp8UZI6Tx5hNnWJyOqvKF6TicCvu3WT7J2dR74A4nKup80NNrRoqI/GVIjMmlF
+t7cRwIbLZccPRwIAhB5Ic6K19IfCWwDT8KsNsxMkcvCgOqXA32AvK6athUzwB7eGIRKfm8aI
+P+pC3eNisSiIfDKjFLSnB47upeWdgtZ8pVK5F3PcbW9RfHm0PWfT/voXJmgoAgBfYNcW3S8
+4oi/xA5ZzgilRhMPPF/g9k1Eb4CRxIaiXzohLLX5K76yuRMZ5PdROsJuV7RSAzoTbHTDNqpQ
+yVlZqRf2bzyxmbVcK8AwEmRnK8te7gfACc4dlzqsITBL+iWBtqnxmY9qY=
+---------------------------------------------------------------------------------
